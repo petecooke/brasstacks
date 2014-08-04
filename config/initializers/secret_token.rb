b8015ac287c4ee52091eb22134e5fbeb824c580f
Pete Cooke <pete.cooke.81@gmail.com>
2014-08-04 23:37:35 +0000

    Initial commit - redo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brasstacks::Application.config.secret_key_base = '450413ae588f3aab7b1a0503a24a99c35fcddfda0839ccdc93a48a6e61207414eb6ec52fcc3b4d9c4568f76aae64c0e0718adb972b1b6c89a85325556f5a51cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brasstacks::Application.config.session_store :cookie_store, key: '_brasstacks_session'
