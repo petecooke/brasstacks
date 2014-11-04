a24b27daeab4a579027fd88091a2ca3002da0fe4
Pete Cooke <pete.cooke.81@gmail.com>
2014-11-04 04:12:00 +0000

    fixed up the race levels page

--- app/models/race_level.rb
+++ app/models/race_level.rb
@@ -1,11 +1,16 @@
 class RaceLevel < ActiveRecord::Base
 
 	scope :distinct_race_levels, -> {
-		joins({:entries => {:race => [:chart,:race_level]}}).
+		joins({:races => [:chart,:race_level]}).
 		group("charts.track_name, race_levels.id").
 		select("charts.track_name, race_levels.race_type, race_levels.restriction, cast(race_levels.distance as int) / 100 as distance, race_levels.dist_unit, race_levels.surface, race_levels.class_rating, race_levels.id").
 		order("race_levels.class_rating DESC")
-	}		
+	}
+
+	scope :distinct_tracks, -> {
+		from("charts").
+		select("track_name").distinct
+	}			
 
 	has_many :races
 
